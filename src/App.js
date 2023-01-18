bcbc4acf8b4e096c876687f33e4f831bab70c93b
Federico Cáceres <federicoenmanuel@gmail.com>
2023-01-18 19:22:49 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  counterClick = () => {
+    this.setState({
+      counter: this.state.counter+1
+    })
+  };
+
   render() {
     return (
       <div>
-        <span className="value">1</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.counterClick}>Incrementa</button>
       </div>
     );
   }
